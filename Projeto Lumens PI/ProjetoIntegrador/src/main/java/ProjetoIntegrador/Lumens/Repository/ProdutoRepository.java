1f5f75b93b05e5f2386de1b5efd8005708cda9e1
Lumens <97890751+projetolumens@users.noreply.github.com>
2022-01-26 12:06:21 -0300

    Add files via upload

---
+++ Projeto Lumens PI/ProjetoIntegrador/src/main/java/ProjetoIntegrador/Lumens/Repository/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package ProjetoIntegrador.Lumens.Repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<ProdutoModel, Long> {
+	public List<ProdutoModel> findAllByProdutoContainingIgnoreCase(String produto);
+}
