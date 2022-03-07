f55e42f16933714a3e6d260ccb037c8ce539c978
unknown <coelhaoyagoo@gmail.com>
2022-03-07 17:52:00 -0300

    Atualizando Back-end

---
+++ Lumens/src/main/java/org/generation/Lumens/LumensApplication.java
@@ -0,0 +1,14 @@
+package org.generation.Lumens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LumensApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(LumensApplication.class, args);
+	}
+
+}
